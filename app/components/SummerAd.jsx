8795df043c7dc6235335c32866445289142f50dc
jib2004 <obasaajibola04@gmail.com>
2024-07-11 23:46:27 +0100

    Corrected error

--- app/components/SummerAd.jsx
+++ app/components/SummerAd.jsx
@@ -9,7 +9,7 @@ const SummerAd = () => {
     <div  className='relative '>
     <div className='h-[270px] md:h-[507px] border bg-[#006200] opacity-60 relative flex flex-col justify-between p-2 md:p-[16px] md:rounded-[24px] md:w-[90%] md:mx-auto mb-[24px]'>
         <h3 className=' font-semibold tracking-[4%] text-white w-[299px] md:w[511px] h-[54px] md:h-[120px] md:text-[25px] leading-[27px] md:leading-[60px]'>Our Curated Summer <br /> Collection</h3>
-        <p className='w-[320px] pr-3 md:p-0  xl:w-[623px] tracking-[4%] leading-[21px] md:leading-[40px] text-[14px] md:text-[24px] text-white'>
+        <p className='w-[320px] pr-3 md:p-0  md:w-[623px] tracking-[4%] leading-[21px] md:leading-[40px] text-[14px] md:text-[24px] text-white'>
         Explore our curated summer collection featuring trending styles, vibrant colors and lightweight fabrics perfect for long days and nights.
         </p>
         <button className=' font-medium w-[90%] md:w-[280px] p-[10px] h-[44px] md:h-[62px] rounded-[8px] bg-white text-[#222222] md:text-[24px]'>Explore Now</button>
