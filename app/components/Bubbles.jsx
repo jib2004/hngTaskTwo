6186e27bf977815a70c0ba59fb536fecfecba517
jib2004 <obasaajibola04@gmail.com>
2024-07-11 23:31:58 +0100

    Changes

--- app/components/Bubbles.jsx
+++ app/components/Bubbles.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const Bubbles = () => {
   return (
     <>
-    <div className='absolute rounded-full -z-10 size-[9px] bg-[#084076] opacity-70 top-[147px] left-[151px]'></div>
-    <div className='absolute rounded-full -z-10 size-[18px] bg-[#222222] opacity-70 top-[173px] left-[186px]'></div>
-    <div className='absolute rounded-full -z-10 opacity-70 size-[25px] bg-[#316E02] top-[156px] left-[257px]'></div>
-    <div className='absolute rounded-full -z-10 opacity-70 size-[9px] bg-[#084076] top-[193px] left-[311px]'></div>
-    <div className='absolute rounded-full -z-10 opacity-70 size-[9px] bg-[#316E02] top-[10px] left-[223px]'></div>
-    <div className='absolute rounded-full -z-10 opacity-70 size-[17px] bg-[#222222] top-[54px] left-[249px]'></div>
-    <div className='absolute rounded-full -z-10 opacity-70 size-[17px] bg-[#CAD3B8] top-[15px] left-[307px]'></div>
-    <div className='absolute rounded-full -z-10 opacity-70 size-[18px] bg-[#222222] top-[53px] left-[349px]'></div>
+    <div className='absolute rounded-full -z-10 size-[9px] xl:size-[22px] bg-[#084076] opacity-70 top-[147px] xl:top-[352px] left-[151px] xl:left-[430px]'></div>
+    <div className='absolute rounded-full -z-10 size-[18px] xl:size-[40px] bg-[#222222] opacity-70 top-[173px] xl:top-[439px] left-[186px] xl:left-[570px]'></div>
+    <div className='absolute rounded-full -z-10 opacity-70 size-[25px] xl:size-[58px] bg-[#316E02] top-[156px] xl:top-[331px] left-[257px] xl:left-[680px]'></div>
+    <div className='absolute rounded-full -z-10 opacity-70 size-[9px] xl:size-[22px] bg-[#084076] top-[193px] xl:top-[457px] left-[311px] xl:left-[807px]'></div>
+    <div className='absolute rounded-full -z-10 opacity-70 size-[9px] xl:size-[22px] bg-[#316E02] top-[10px] xl:top-[38px] left-[223px] xl:left-[599px]'></div>
+    <div className='absolute rounded-full -z-10 opacity-70 size-[17px] xl:size-[40px] bg-[#222222] top-[54px] xl:top-[144px] left-[249px] xl:left-[698px]'></div>
+    <div className='absolute rounded-full -z-10 opacity-70 size-[17px] xl:size-[40px] bg-[#CAD3B8] top-[15px] xl:top-[40px] left-[307px] xl:left-[796px]'></div>
+    <div className='absolute rounded-full -z-10 opacity-70 size-[18px] xl:size-[40px] bg-[#222222] top-[53px] xl:top-[100px] left-[349px] xl:left-[896px]'></div>
     </>
   )
 }
